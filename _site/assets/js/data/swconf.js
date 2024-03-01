e15520638054d5da610b9fc6e38fcc76066ea636
sjy2335 <joey110951@gmail.com>
2024-03-01 22:05:54 +0900

    build...

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709297408',resources: [
+    cacheName: 'chirpy-1709298171',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
