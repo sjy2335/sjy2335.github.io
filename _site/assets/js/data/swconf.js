5db13d5678cfae6a0aa5ee00448315281709b20d
sjy2335 <joey110951@gmail.com>
2024-03-01 18:17:16 +0900

    blog settings

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709283697',resources: [
+    cacheName: 'chirpy-1709284487',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
